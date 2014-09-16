5d7ba018c5630ea2a98941101c5efb1be1d9f0b0
wangjiangjiao <wangjiangjiao@wangjiangjiaodeMacBook-Pro.local>
2014-09-16 11:10:28 +0800

    Initial Commit

---
+++ AutoLayOutDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayOutDemo
+//
+//  Created by wangjiangjiao on 14-9-16.
+//  Copyright (c) 2014年 elong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
