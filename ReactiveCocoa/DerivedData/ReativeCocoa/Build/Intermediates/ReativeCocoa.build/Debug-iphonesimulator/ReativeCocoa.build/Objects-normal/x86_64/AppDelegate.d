bbaa8bd2526f568c7301e496976a12f8aa572cfa
wangjiangwen <1058881673@qq.com>
2015-09-17 01:03:03 +0800

    ReactiveCocoa

--- ReactiveCocoa/DerivedData/ReativeCocoa/Build/Intermediates/ReativeCocoa.build/Debug-iphonesimulator/ReativeCocoa.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/wangjiangjiao/Desktop/GitHub/GitExerciseDemo/ReactiveCocoa/ReativeCocoa/AppDelegate.m \
-  /Users/wangjiangjiao/Desktop/GitHub/GitExerciseDemo/ReactiveCocoa/ReativeCocoa/AppDelegate.h
--- ReactiveCocoa/DerivedData/ReativeCocoa/Build/Intermediates/ReativeCocoa.build/Debug-iphonesimulator/ReativeCocoa.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/wangjiangjiao/Desktop/GitHub/GitExerciseDemo/ReactiveCocoa/ReativeCocoa/main.m \
-  /Users/wangjiangjiao/Desktop/GitHub/GitExerciseDemo/ReactiveCocoa/ReativeCocoa/AppDelegate.h
